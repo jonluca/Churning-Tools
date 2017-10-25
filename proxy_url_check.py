6a0431065e9d24c88072f9d5ded1c998cd17f7ad
JonLuca DeCaro <jdecaro@usc.edu>
2017-10-25 14:21:26 -0700

    better indentation

--- proxy_url_check.py
+++ proxy_url_check.py
@@ -6,57 +6,15 @@ from fake_useragent import UserAgent
 
 ua = UserAgent()
 
-
 # Update this with 20 - 100 proxies from https://free-proxy-list.net/ on every run
 # If your URL requires https, make sure you filter proxies on whether they support it
-list_of_proxies = [
-	"210.212.73.61:80",
-"35.161.5.60:3128",
-"203.74.4.3:80",
-"204.12.155.204:3128",
-"203.74.4.6:80",
-"117.4.246.95:8080",
-"203.74.4.4:80",
-"110.78.148.68:52305",
-"46.229.136.202:8080",
-"149.255.154.4:8080",
-"203.74.4.1:80",
-"163.172.217.103:3128",
-"109.71.181.234:53281",
-"189.206.107.6:8080",
-"89.236.17.106:3128",
-"189.111.253.20:3128",
-"212.117.19.215:62225",
-"203.74.4.2:80",
-"203.146.82.253:3128",
-"80.83.20.14:80",
-"203.153.113.226:52335",
-"198.50.219.232:8080",
-"115.79.43.156:8888",
-"185.42.223.194:3128",
-"203.74.4.5:80",
-"195.154.42.249:3128",
-"45.32.19.109:3128",
-"52.186.124.212:3128",
-"213.136.89.121:80",
-"58.26.10.67:8080",
-"62.255.12.3:80",
-"213.136.77.246:80",
-"203.74.4.7:80",
-"45.77.136.105:80",
-"62.182.207.26:53281",
-"171.101.236.116:3128",
-"45.6.216.66:3128",
-"143.0.188.26:80",
-"203.74.4.0:80",
-"47.91.235.15:80"
-]
+list_of_proxies = ["210.212.73.61:80", "35.161.5.60:3128", "203.74.4.3:80", "204.12.155.204:3128", "203.74.4.6:80", "117.4.246.95:8080", "203.74.4.4:80", "110.78.148.68:52305", "46.229.136.202:8080", "149.255.154.4:8080", "203.74.4.1:80", "163.172.217.103:3128", "109.71.181.234:53281", "189.206.107.6:8080", "89.236.17.106:3128", "189.111.253.20:3128", "212.117.19.215:62225", "203.74.4.2:80", "203.146.82.253:3128", "80.83.20.14:80", "203.153.113.226:52335", "198.50.219.232:8080", "115.79.43.156:8888", "185.42.223.194:3128", "203.74.4.5:80", "195.154.42.249:3128", "45.32.19.109:3128", "52.186.124.212:3128", "213.136.89.121:80", "58.26.10.67:8080", "62.255.12.3:80", "213.136.77.246:80", "203.74.4.7:80", "45.77.136.105:80", "62.182.207.26:53281", "171.101.236.116:3128", "45.6.216.66:3128", "143.0.188.26:80", "203.74.4.0:80", "47.91.235.15:80"]
 
 # Globals
 proxy = list_of_proxies[0]
 connectionPerSec = 50
 
-#Useful combinations
+# Useful combinations
 num = '0123456789'
 a_z = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 numAZ = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
@@ -67,10 +25,11 @@ end = "&cellNumber="
 lines = []
 current_line = 0
 
+
 # Creates all URL combinations based on parameters above and stores them in a lines global var
 def generate_urls():
 	for (a) in map(''.join, itertools.product(num, repeat=4)):
-		#for (b) in itertools.imap(''.join, itertools.product(numAZ, repeat=2)):
+		# for (b) in itertools.imap(''.join, itertools.product(numAZ, repeat=2)):
 		for (cell) in map(''.join, itertools.product(num, repeat=1)):
 			line = front
 			line += a
@@ -83,6 +42,7 @@ def generate_urls():
 valid_url_file_lock = threading.Lock()
 invalid_url_file_valid_url_file_lock = threading.Lock()
 
+
 # Often times, the script will need to be interrupted midway through. This will remove all invalid from the testing lines
 # Pretty fast (roughly 20s to check 100,000 urls)
 def remove_completed():
@@ -91,6 +51,7 @@ def remove_completed():
 			temp_line = line[5:]
 			if temp_line.rstrip("\n") in lines:
 				lines.remove(temp_line.rstrip("\n"))
+
 
 # Write a valid url to file
 def write_valid_url(res):
@@ -101,6 +62,7 @@ def write_valid_url(res):
 		myfile.write('\n')
 	valid_url_file_lock.release()
 
+
 # Writes an invalid url to file
 def write_invalid_url(ret):
 	invalid_url_file_valid_url_file_lock.acquire()  # thread bvalid_url_file_locks at this line until it can obtain valid_url_file_lock
@@ -110,6 +72,7 @@ def write_invalid_url(ret):
 		myfile.write('\n')
 	invalid_url_file_valid_url_file_lock.release()
 
+
 def retry(ret):
 	invalid_url_file_valid_url_file_lock.acquire()  # thread bvalid_url_file_locks at this line until it can obtain valid_url_file_lock
 
@@ -117,6 +80,7 @@ def retry(ret):
 		myfile.write(ret)
 		myfile.write('\n')
 	invalid_url_file_valid_url_file_lock.release()
+
 
 def get_url():
 	global current_line
@@ -127,10 +91,7 @@ def get_url():
 
 
 def generate_req(reqSession):
-	headers = {
-		"Connection": "close",
-		'User-Agent': ua.random
-	}
+	headers = {"Connection": "close", 'User-Agent': ua.random}
 	proxy = list_of_proxies[0]
 
 	while True:
@@ -160,7 +121,6 @@ def generate_req(reqSession):
 
 
 def main():
-
 	generate_urls()
 	remove_completed()
 
@@ -170,10 +130,7 @@ def main():
 	# generate threads based on conccurent connections global
 	for cnum in range(connectionPerSec):
 		s1 = requests.session()
-		th = threading.Thread(
-			target=generate_req, args=(s1,),
-			name='thread-{:03d}'.format(cnum),
-		)
+		th = threading.Thread(target=generate_req, args=(s1,), name='thread-{:03d}'.format(cnum), )
 		th.start()
 
 	for th in threading.enumerate():
